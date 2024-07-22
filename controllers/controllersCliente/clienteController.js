8f7edcbf9e0c140eb2d8b479ef203dd6c5c067be
Gary Alexander Campusano Paredes <Ingcampusano@Outlook.com>
2024-07-22 16:35:50 -0400

    GC - Agrugue las el controlador  del cliente

---
+++ controllers/controllersCliente/clienteController.js
@@ -0,0 +1,30 @@
+exports.getHome = (req, res, next) => {
+  res.render("viewsCliente/home", {
+    pageTitle: "App Cenar | Cliente",
+    layout: "layoutCliente",
+  });
+};
+exports.getDirecciones = (req, res, next) => {
+  res.render("viewsCliente/viewDirecciones", {
+    pageTitle: "App Cenar | Direcciones",
+    layout: "layoutCliente",
+  });
+};
+exports.getFavoritos = (req, res, next) => {
+  res.render("viewsCliente/viewFavoritos", {
+    pageTitle: "App Cenar | Favoritos",
+    layout: "layoutCliente",
+  });
+};
+exports.getPerfil = (req, res, next) => {
+  res.render("viewsCliente/viewPerfil", {
+    pageTitle: "App Cenar | Perfil",
+    layout: "layoutCliente",
+  });
+};
+exports.getPedidos = (req, res, next) => {
+  res.render("viewsCliente/viewPedidos", {
+    pageTitle: "App Cenar | Pedidos",
+    layout: "layoutCliente",
+  });
+};
