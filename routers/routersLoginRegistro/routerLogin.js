944d0a5daad07319873e08c28da87c974e55181b
Gary Alexander Campusano Paredes <Ingcampusano@Outlook.com>
2024-07-23 10:25:18 -0400

    GC - Refactor - Puse /login como ruta principal

--- routers/routersLoginRegistro/routerLogin.js
+++ routers/routersLoginRegistro/routerLogin.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const loginController = require("../../controllers/controllersLoginRegistro/loginController");
 
-router.get("/login", loginController.getLogin);
+router.get("/", loginController.getLogin);
 router.post("/login", loginController.PostLogin);
 
 module.exports = router;
