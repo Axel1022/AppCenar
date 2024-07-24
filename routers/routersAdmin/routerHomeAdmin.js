7f6a58aab187d35b060d68e5e6c8a8b9d3c22132
Gary Alexander Campusano Paredes <Ingcampusano@Outlook.com>
2024-07-24 11:46:41 -0400

    GC - Corregi la ruta para el admin /admin/home

--- routers/routersAdmin/routerHomeAdmin.js
+++ routers/routersAdmin/routerHomeAdmin.js
@@ -1,5 +1,5 @@
 const express = require("express");
 const router = express.Router();
 const homeController = require("../../controllers/controllersAdmin/homeController");
-router.get("/homeAdmin", homeController.getHome);
+router.get("/admin/home", homeController.getHome);
 module.exports = router;
