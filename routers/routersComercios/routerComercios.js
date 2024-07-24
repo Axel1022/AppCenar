184634b243e5feb11af0819be7d03d9ac4d5ed3a
Gary Alexander Campusano Paredes <Ingcampusano@Outlook.com>
2024-07-24 10:45:49 -0400

    GC - Rruta de comercios

---
+++ routers/routersComercios/routerComercios.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const comerciosController = require("../../controllers/controllersComercios/comerciosController");
+router.get("/comercios/bebidas", comerciosController.getViewBebidas);
+router.get("/comercios/mercados", comerciosController.getViewMercados);
+router.get("/comercios/postres_cafe", comerciosController.getViewPostres_Cafe);
+router.get("/comercios/restaurantes", comerciosController.getViewRestaurantes);
+router.get("/comercios/salud", comerciosController.getViewSalud);
+router.get("/comercios/tiendas", comerciosController.getViewTiendas);
+module.exports = router;
