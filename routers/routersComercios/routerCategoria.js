ee8f8336070bfaa30f40a7c0c402eeec2fb866b8
Gary Alexander Campusano Paredes <Ingcampusano@Outlook.com>
2024-07-24 22:21:57 -0400

    GC - Rutas corregidas

--- routers/routersComercios/routerCategoria.js
+++ routers/routersComercios/routerCategoria.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const categoriaController = require("../../controllers/controllersComercios/categoriaController");
+const categoriaController = require("../../controllers/controllersCategorias/categoriaController");
 
 router.get("/comercios/Categorias", categoriaController.GetCategoria);
 router.get("/comercios/AgregarCategoria", categoriaController.GetAddCategoria);
