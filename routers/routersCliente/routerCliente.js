be7ad72446a69d1de527d7032ebc9b2552df1594
Gary Alexander Campusano Paredes <Ingcampusano@Outlook.com>
2024-07-25 23:00:37 -0400

    GC - Ruta para agregar direcciones

--- routers/routersCliente/routerCliente.js
+++ routers/routersCliente/routerCliente.js
@@ -5,6 +5,11 @@ const clienteController = require("../../controllers/controllersCliente/clienteC
 
 router.get("/cliente/home", clienteController.getHome);
 router.get("/cliente/direcciones", clienteController.getDirecciones);
+router.get("/cliente/direcciones/add", clienteController.getDireccionesAdd);
+router.post(
+  "/cliente/direcciones/add/post",
+  clienteController.postDireccionesAdd
+);
 router.get("/cliente/favoritos", clienteController.getFavoritos);
 router.get("/cliente/perfil", clienteController.getPerfil);
 router.get("/cliente/perfil/editar", clienteController.getEditPerfil);
