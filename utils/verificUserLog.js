cbe09265ce5d0b85af3205c6d8c09460e6fac2db
Gary Alexander Campusano Paredes <Ingcampusano@Outlook.com>
2024-07-31 23:44:17 -0400

    GC - Cambien el redirecion a /login

--- utils/verificUserLog.js
+++ utils/verificUserLog.js
@@ -1,7 +1,6 @@
 module.exports = function (req, res, next) {
   if (!req.session || !req.session.user) {
     req.flash("errors", "Sesion expirada");
-    return res.redirect("http://localhost:8080/login");
-  }
-  return req.session.user.id;
+    return res.redirect("/login");
+  } else return req.session.user.id;
 };
