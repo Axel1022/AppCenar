dd3876202f65265aad7f949cdc2eeb66977f4d09
Gary Alexander Campusano Paredes <Ingcampusano@Outlook.com>
2024-08-05 02:28:49 -0400

    General

--- controllers/controllersAdmin/adminController.js
+++ controllers/controllersAdmin/adminController.js
@@ -1,109 +1,99 @@
+const ModelCliente = require("../../models/modelCliente/cliente");
+const ModelDelivery = require("../../models/modelDelivery/delivery");
+const ModelComercio = require("../../models/modelComercios/comercio");
+const ModelAdministrador = require("../../models/modelAdmin/administrador");
+const ModelProducto = require("../../models/modelComercios/producto");
+const ModelPedido = require("../../models/modelCliente/pedido");
+const verific = require("../../utils/verificUserLog");
 
-const Cliente = require("../../models/modelCliente/cliente");
-const Delivery = require("../../models/modelDelivery/delivery");
-const Comercio = require("../../models/modelComercios/comercio");
-const Administrador = require("../../models/modelAdmin/administrador");
-const Producto  = require("../../models/modelComercios/producto");
+exports.getClientes = async (req, res, next) => {
+  verific(req, res, next);
 
-exports.getHome = async (req, res) => {
-    try {
-        const comerciosActivos = await Comercio.count({ where: { active: true } });
-        const comerciosInactivos = await Comercio.count({ where: { active: false } });
-        const clientesActivos = await Cliente.count({ where: { active: true } });
-        const clientesInactivos = await Cliente.count({ where: { active: false } });
-        const deliveryActivos = await Delivery.count({ where: { active: true } });
-        const deliveryInactivos = await Delivery.count({ where: { active: false } });
-        const totalProductos = await Producto.count();
 
-        res.render('admin/home', {
-            totalPedidos,
-            pedidosHoy,
-            comerciosActivos,
-            comerciosInactivos,
-            clientesActivos,
-            clientesInactivos,
-            deliveryActivos,
-            deliveryInactivos,
-            totalProductos
-        });
-    } catch (error) {
-        res.status(500).send('Error al obtener las estadísticas');
-    }
-};
+    const clientes = await items.map((cliente) => {
 
-exports.getClientes = async (req, res) => {
-    try {
-        const clientes = await Cliente.findAll();
+        const itemsPedidos = ModelPedido.findAll({ where: { id: cliente.id } });
 
-        res.render('viewsAdmin/listadoCliente', { clientes });
-    } catch (error) {
-        res.status(500).send('Error al obtener el listado de clientes');
-    }
+    });
+  res.render("viewsAdmin/listadoCliente", {
+    pageTitle: "Food Rush | Admin",
+    layout: "layoutAdmin",
+    Clientes: clientes,
+    has: clientes.length,
+  });
 };
 
 exports.getDeliveries = async (req, res) => {
-    try {
-        const deliveries = await Delivery.findAll();
+  try {
+    const deliveries = await Delivery.findAll();
 
-        res.render('viewsAdmin/listadoDelivery', { deliveries });
-    } catch (error) {
-        res.status(500).send('Error al obtener el listado de deliveries');
-    }
+    res.render("viewsAdmin/listadoDelivery", { deliveries });
+  } catch (error) {
+    res.status(500).send("Error al obtener el listado de deliveries");
+  }
 };
 
 exports.getComercios = async (req, res) => {
-    try {
-        const comercios = await Comercio.findAll();
+  try {
+    const comercios = await Comercio.findAll();
 
-        res.render('viewsAdmin/listadoComercio', { comercios });
-    } catch (error) {
-        res.status(500).send('Error al obtener el listado de comercios');
-    }
+    res.render("viewsAdmin/listadoComercio", { comercios });
+  } catch (error) {
+    res.status(500).send("Error al obtener el listado de comercios");
+  }
 };
 
-
-
 exports.getAdministradores = async (req, res) => {
-    try {
-        const administradores = await Administrador.findAll();
-        res.render('viewsAdmin/mantenimientoAdmin', { administradores });
-    } catch (error) {
-        res.status(500).send('Error al obtener el listado de administradores');
-    }
+  try {
+    const administradores = await Administrador.findAll();
+    res.render("viewsAdmin/mantenimientoAdmin", { administradores });
+  } catch (error) {
+    res.status(500).send("Error al obtener el listado de administradores");
+  }
 };
 
 exports.createAdministrador = async (req, res) => {
-    try {
-        const { name, lastName, identification, email, user, password } = req.body;
-        await Administrador.create({ name, lastName, identification, email, user, password });
-        res.redirect('/viewsAdmin/crearAdmin');
-    } catch (error) {
-        res.status(500).send('Error al crear el administrador');
-    }
+  try {
+    const { name, lastName, identification, email, user, password } = req.body;
+    await Administrador.create({
+      name,
+      lastName,
+      identification,
+      email,
+      user,
+      password,
+    });
+    res.redirect("/viewsAdmin/crearAdmin");
+  } catch (error) {
+    res.status(500).send("Error al crear el administrador");
+  }
 };
 
 exports.updateAdministrador = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const { name, lastName, identification, email, user, password } = req.body;
-        await Administrador.update({ name, lastName, identification, email, user, password }, { where: { id } });
-        res.redirect('/admin/administradores');
-    } catch (error) {
-        res.status(500).send('Error al actualizar el administrador');
-    }
+  try {
+    const { id } = req.params;
+    const { name, lastName, identification, email, user, password } = req.body;
+    await Administrador.update(
+      { name, lastName, identification, email, user, password },
+      { where: { id } }
+    );
+    res.redirect("/admin/administradores");
+  } catch (error) {
+    res.status(500).send("Error al actualizar el administrador");
+  }
 };
 
 exports.deleteAdministrador = async (req, res) => {
-    try {
-        const { id } = req.params;
-        await Administrador.destroy({ where: { id } });
-        res.redirect('/admin/administradores');
-    } catch (error) {
-        res.status(500).send('Error al eliminar el administrador');
-    }
+  try {
+    const { id } = req.params;
+    await Administrador.destroy({ where: { id } });
+    res.redirect("/admin/administradores");
+  } catch (error) {
+    res.status(500).send("Error al eliminar el administrador");
+  }
 };
 
 exports.logout = (req, res) => {
-    req.session.destroy();
-    res.redirect('/login');
+  req.session.destroy();
+  res.redirect("/login");
 };
